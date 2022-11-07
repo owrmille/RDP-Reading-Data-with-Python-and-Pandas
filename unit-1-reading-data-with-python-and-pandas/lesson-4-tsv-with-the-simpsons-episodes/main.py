947b729e4ffad8ab197ac88cf421c35c0aa44b77
Yasemin Topchu <y.topchu@inbox.ru>
2022-11-07 01:20:32 +0100

    Added my solution

--- unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
@@ -1,7 +1,21 @@
 import numpy as np
 import pandas as pd
+from tests.test_simpsons_columns import test_simpsons_columns
+from tests.test_simpsons_item import test_simpsons_item
+from tests.test_simpsons_shape import test_simpsons_shape
 
 col_names = ['Title', 'Air date', 'Production code', 'Season', 'Number in season',
              'Number in series', 'US viewers (million)', 'Views', 'IMDB rating']
              
-                          
+simpsons = pd.read_csv('files/simpsons-episodes.tsv', 
+                        delimiter='\t',
+                        names=col_names,
+                        usecols=['Title', 'Air date', 'Production code', 'IMDB rating'],
+                        na_values='no_val',
+                        skiprows=4,
+                        index_col='Production code',
+                        parse_dates=['Air date'])      
+
+test_simpsons_shape(simpsons)
+test_simpsons_columns(simpsons)
+test_simpsons_item(simpsons)                   
